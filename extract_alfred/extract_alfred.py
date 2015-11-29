66c01b8845a9e9579a5b0c754c824c43d23df703
Christian Roeder <christian.roeder@red-hood.de>
2015-11-30 00:23:46 +0100

    write full node entry

--- extract_alfred/extract_alfred.py
+++ extract_alfred/extract_alfred.py
@@ -4,45 +4,25 @@ import json
 import time as time_mod
 import logging
 from argparse import ArgumentParser
-from functools import reduce
 
 import config
 
 logging.basicConfig(level=logging.WARNING)
 
-
-def get_recursive(path, p_dict):
-    logging.debug("Getting path %s from obj %s", path, p_dict)
-    return reduce(dict.__getitem__, path.split("."), p_dict)
-
-
 # TODO create dict class with versioning instead of explicit access
-def create_or_update(target, src, target_key, src_key, time):
-    versions = target.get(target_key, [])
-
-    try:
-        val = get_recursive(src_key, src)
-    # it's not an error if key does not exists
-    # TODO treat it individually for different keys
-    except KeyError:
-        return
-    #also handle TypeError as some nodes have non-expected types here
-    except TypeError as e:
-        logging.warning(repr(e))
-    else:
-        target[target_key] = make_entry(versions, val, time)
 
 
-def make_entry(versions, val, time):
+def _get_sorted_versions(versions):
+    return sorted(versions, key=lambda x: x[1])
+
+
+def create_or_update(versions, val, time):
     # we assume sorted list here
     if len(versions) > 0 and versions[-1][0] == val:
         return versions
     versions.append([val, time])
+    versions = _get_sorted_versions(versions)
     return versions
-
-
-def get_current(versioned_dict, key):
-    return sorted(versioned_dict[key].items(), key=lambda x: x[1])[0]
 
 
 def dump_path(obj, path):
@@ -61,10 +41,8 @@ def load_path(path):
 
 def process_items(nodes, db, mapping, time):
     for k, node_entry in nodes.items():
-        db_entry = db.get(k, {})
-        for target_key, src_key in mapping.items():
-            create_or_update(db_entry, node_entry, target_key, src_key, time)
-        db[k] = db_entry
+        versions = db.get(k, [])
+        db[k] = create_or_update(versions, node_entry, time)
 
 
 def run(nodes_file, db_file, time):
