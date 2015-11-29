c1417cc949682dec76f71d2f83600d14ac659cef
Christian Roeder <christian.roeder@red-hood.de>
2015-11-29 16:11:33 +0100

    preliminary version

--- extract_alfred/config.py
+++ extract_alfred/config.py
@@ -1,5 +1,10 @@
 KEY_MAPPING = {
     'node_id': 'node_id',
     'contact': 'owner.contact',
-    'hostname': 'hostname'
+    'hostname': 'hostname',
+    'model': 'hardware.model',
+    'longitude': 'location.longitude',
+    'latitude': 'location.latitude',
+    'base': 'software.firmware.base',
+    'release': 'software.firmware.release'
 }
--- extract_alfred/extract_alfred.py
+++ extract_alfred/extract_alfred.py
@@ -1,38 +1,43 @@
 #!/bin/env python
 
-import sys
 import json
-import datetime
-from bson import json_util
+import time as time_mod
+import logging
+from argparse import ArgumentParser
 from functools import reduce
 
 import config
 
-dt = datetime.datetime
-argv = sys.argv
+logging.basicConfig(level=logging.WARNING)
 
 
 def get_recursive(path, p_dict):
+    logging.debug("Getting path %s from obj %s", path, p_dict)
     return reduce(dict.__getitem__, path.split("."), p_dict)
 
 
 # TODO create dict class with versioning instead of explicit access
-def create_or_update(target, src, target_key, src_key):
+def create_or_update(target, src, target_key, src_key, time):
     versions = target.get(target_key, [])
 
     try:
         val = get_recursive(src_key, src)
+    # it's not an error if key does not exists
+    # TODO treat it individually for different keys
     except KeyError:
         return
+    #also handle TypeError as some nodes have non-expected types here
+    except TypeError as e:
+        logging.warning(repr(e))
     else:
-        target[target_key] = make_entry(versions, val)
+        target[target_key] = make_entry(versions, val, time)
 
 
-def make_entry(versions, val):
+def make_entry(versions, val, time):
     # we assume sorted list here
     if len(versions) > 0 and versions[-1][0] == val:
-            return versions
-    versions.append([val, dt.utcnow()])
+        return versions
+    versions.append([val, time])
     return versions
 
 
@@ -45,33 +50,39 @@ def dump_path(obj, path):
         json.dump(
             obj,
             fp,
-            default=json_util.default,
             sort_keys=True,
             indent=4)
 
 
 def load_path(path):
     with open(path, "r") as fp:
-        return json.load(fp, object_hook=json_util.object_hook)
+        return json.load(fp)
 
 
-def process_items(nodes, db, mapping):
+def process_items(nodes, db, mapping, time):
     for k, node_entry in nodes.items():
         db_entry = db.get(k, {})
         for target_key, src_key in mapping.items():
-            create_or_update(db_entry, node_entry, target_key, src_key)
+            create_or_update(db_entry, node_entry, target_key, src_key, time)
         db[k] = db_entry
 
 
-def run(nodes_file, db_file):
+def run(nodes_file, db_file, time):
     nodes = load_path(nodes_file)
     try:
         db = load_path(db_file)
     except:
         db = {}
-    process_items(nodes, db, config.KEY_MAPPING)
+    process_items(nodes, db, config.KEY_MAPPING, time)
     dump_path(db, db_file)
 
 
 if __name__ == "__main__":
-    run(argv[1], argv[2])
+    cur_time = int(time_mod.time())
+    parser = ArgumentParser()
+    parser.add_argument("input")
+    parser.add_argument("output")
+    parser.add_argument("--time", default=cur_time)
+    args = parser.parse_args()
+
+    run(args.input, args.output, args.time)
