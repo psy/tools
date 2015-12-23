ce6eaab25545595b04765094b19d16b6ed303937
Andreas Rammhold <andreas@rammhold.de>
2015-12-23 17:25:35 +0100

    Added update-telemetry

---
+++ update-telemetry.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+import json
+import requests
+import re
+import itertools
+from contextlib import contextmanager
+import pprint
+import time
+import socket
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@contextmanager
+def get_socket(host, port):
+    sock = socket.socket()
+    sock.settimeout(1)
+    sock.connect((host, port))
+    yield sock
+    sock.close()
+
+
+def write_to_graphite(data, prefix='freifunk', hostname=socket.gethostname()):
+    now = time.time()
+    with get_socket('graphite', 2013) as s:
+        for key, value in data.items():
+            line = "%s.%s.%s %s %s\n" % (prefix, hostname, key, value, now)
+            s.sendall(line.encode('latin-1'))
+
+def main():
+    device_name_mapping = {
+        'freifunk': 'ffda-br',
+        'bat0': 'ffda-bat',
+        'mesh-vpn': 'ffda-vpn'
+    }
+    device_whitelist = [
+        'eth0',
+        'tun-ffrl-ber',
+        'tun-ffrl-dus',
+        'tun-ffrl-fra',
+        'tun-ffda-gw01',
+        'tun-ffda-gw02',
+        'tun-ffda-gw03',
+        'tun-ffda-gw04',
+        'ffda-vpn',
+        'ffda-bat',
+        'ffda-br',
+        'icvpn'
+    ]
+    fields = [
+        'bytes', 'packets', 'errs', 'drop', 'fifo',
+        'frame', 'compressed', 'multicast',
+    ]
+    field_format = '(?P<{direction}_{field}>\d+)'
+    
+    pattern = re.compile(
+        '^\s*(?P<device_name>[\w-]+):\s+' + '\s+'.join(
+            itertools.chain.from_iterable((field_format.format(direction=direction, field=field)
+                                           for field in fields) for direction in ['rx', 'tx'])
+        )
+    )
+
+    update = {}
+    with open('/proc/net/dev') as fh:
+        lines = fh.readlines()
+        for line in lines:
+            m = pattern.match(line)
+            if m:
+                groupdict = m.groupdict()
+                device_name = groupdict.pop('device_name')
+                device_name = device_name_mapping.get(device_name, device_name)
+                if device_name in device_whitelist:
+                    for key, value in groupdict.items():
+                        direction, metric = key.split('_')
+                        update['%s.%s.%s' % (device_name, direction, metric)] = value
+
+    with open('/proc/loadavg', 'r') as fh:
+        line = fh.read()
+        values = line.split(' ', 3)
+        update['load.15'] = values[0]
+        update['load.5'] = values[1]
+        update['load.1'] = values[2]
+
+    for key in  ['count', 'max']:
+        try:
+            with open('/proc/sys/net/netfilter/nf_conntrack_%s' % key, 'r') as fh:
+                update['netfilter.%s' % key] = fh.read().strip()
+        except IOError as e:
+            pass
+
+              
+
+    #pprint.pprint(update)
+    write_to_graphite(update)
+
+
+if __name__ == "__main__":
+    main()
